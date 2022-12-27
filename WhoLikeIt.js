444847aa3bf0cede1ba6105e447b38bd2dc03ac6
SpensirF <Spensirfields@gmail.com>
2022-12-27 12:31:42 -0800

    who like it algo

---
+++ WhoLikeIt.js
@@ -0,0 +1,19 @@
+//output - " 'name from array' likes this "- string 
+//input - 'arr of names' 
+//constraint - na
+//edge cases - na 
+
+function likes(names) {
+    if (names.length == 0) {
+        return "no one likes this";
+        } else if (names.length == 1) {
+        return names[0] + " likes this";  
+        } else if (names.length == 2) {
+        return names[0] + " and " + names[1] + " like this";
+        } else if (names.length == 3) {
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        } else {
+        return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        }
+}
+console.log(likes(["Max", "John", "Mark"]) ) 
