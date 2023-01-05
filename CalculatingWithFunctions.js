1c2ed43514be0f7f7ed51cf368edf40f59fe017f
SpensirF <Spensirfields@gmail.com>
2023-01-05 12:19:43 -0800

    Calculating with functions && Twice as Old

---
+++ CalculatingWithFunctions.js
@@ -0,0 +1,15 @@
+function zero(x) {return x ? x(0) :0 }
+function one(x) {return x ? x(1) :1 }
+function two(x) {return x ? x(2) :2 }
+function three(x) {return x ? x(3) :3 }
+function four(x) {return x ? x(4) :4 }
+function five(x) {return x ? x(5) :5 }
+function six(x) {return x ? x(6) :6 }
+function seven(x) {return x ? x(7) :7 }
+function eight(x) {return x ? x(8) :8 }
+function nine(x) {return x ? x(9) :9 }
+
+function plus(b) {return (a)=> a + b}
+function minus(b) {return (a)=> a - b}
+function times(b) {return (a)=> a * b}
+function dividedBy(b) {return (a)=> a / b}
---
+++ twiceAsOld.js
@@ -0,0 +1,16 @@
+//o - int of the age the father was double 
+//i - 2 ints Сalculate how many years ago the father was twice as old as his son
+//c- 
+//e 
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let son = sonYearsOld * 2
+    let dad = dadYearsOld - son
+    return Math.abs(dad)
+  
+}
+
+console.log(twiceAsOld(36,7))
+//   it("Testing for dad's age: 36 and son's age: 7", function(){
+//     assert.strictEqual(twiceAsOld(36,7) , 22);
