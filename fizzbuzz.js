14d1f3cadb4812e95fabdb50203cb58239857e5f
spensirF <spensirfields@gmail.com>
2023-01-26 16:09:15 -0800

    fizzbuzz algo

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,9 @@
 function fizzBuzz(n) {
-    for(let i =1; i<=n; i++){
+
+    for(let i = 1; i <= n; i++){
         if(i % 15 === 0){
             console.log("FizzBuzz") 
-        }else if(i%3 ===0){
+        }else if(i % 3 ===0){
             console.log("Fizz") 
         }else if(i % 5 === 0){
             console.log("Buzz") 
