8cbce939bc62645c32841bac33d91121f78a70d2
spensirF <spensirfields@gmail.com>
2023-02-27 13:55:52 -0800

    summation algo

--- DebugSayHello.js
+++ DebugSayHello.js
@@ -1,4 +1,4 @@
 function sayHello(name) {
-    return 'Hello,' + " " + name
+    return 'Hello, ' + name
 }
 console.log(sayHello('Mr. Spock')) 
---
+++ summation.js
@@ -0,0 +1,9 @@
+function summation(nums){
+    let sum = 0 
+    for(i = 1; i <= nums; i++){
+        
+        sum += i 
+    }
+    return sum 
+}
+console.log(summation(8)) 
